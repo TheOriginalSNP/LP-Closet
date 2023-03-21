b9d519671d123c861623db1aa02fb1eeb79313d1
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-21 19:34:00 -0400

    Add hero and featured section with mongo db

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 //jshint esversion:6
 
-
+const path = require('path');
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
 const express = require("express");
 const app = express();
 
+const heroTitle = "Check out our new Summer Collection";
+const heroMessage = "Get your furry friend ready for summer! Check out our new Summer Selection of stylish dog clothes and accessories today. From cooling vests to trendy bandanas, we've got everything you need to keep your pup looking and feeling great all season long. Shop now and treat your pup to the best summer yet!";
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect("mongodb+srv://KuraiOG:XyHYvVb5Y80KPC6w@cluster0.1nx30vc.mongodb.net/LPCHomeData", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to MDB"));
 
+const featuredCardSchema = new mongoose.Schema({
+    title: String,
+    tag: String,
+    image: String
+});
 
+const Card = mongoose.model("Card", featuredCardSchema, "FeaturedCards");
 
-app.get("/", function (req, res) {
-    res.render("home")
+app.get('/', async (req, res) => {
+    const cards = await Card.find().limit(4);
+    res.render("home", {
+        content: heroTitle,
+        message: heroMessage,
+        cards: cards
+    });
 });
 
 app.post("/", function (req, res) {
@@ -24,5 +45,5 @@ app.post("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-    console.log("Sever 3000 up and running!");
+    console.log("Server 3000 up and running!");
 });
